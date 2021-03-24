9908d025e19269e3e3849dc2aa3b9d2956209876
FranKeN Pereira <frankenpereira@gmail.com>
2021-03-24 21:13:14 +0000

    auto achievements virefied

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ countDays()
 function oneMonth(){
   const today = new Date().getTime();
   const dateOne = new Date(`April 06 2020 00:00:00 `).getTime();
-  const dateTwo = new Date(`May 06 2020 00:00:00 `).getTime();
+  const dateTwo = new Date(`April 07 2020 00:00:00 `).getTime();
 
   if(today >= dateOne){
     setAsPassedOne()
