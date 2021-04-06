5e0c3922ecb7097f6bfde80e0b447797ba3bf9a7
FranKeN Pereira <frankenpereira@gmail.com>
2021-04-06 15:45:54 +0100

    update on dates and bugs fix

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,72 @@ async function setAsPassedTwo(){
   date2.setAttribute('class', 'dateDone')
 }
 
+async function setAsPassedTree(){
+  var item2 = document.querySelector('h2.item3')
+  var date2 = document.querySelector('p.date3')
+
+  item2.setAttribute('class', 'itemDone')
+  date2.setAttribute('class', 'dateDone')
+}
+async function setAsPassedFour(){
+  var item2 = document.querySelector('h2.item4')
+  var date2 = document.querySelector('p.date4')
+
+  item2.setAttribute('class', 'itemDone')
+  date2.setAttribute('class', 'dateDone')
+}
+async function setAsPassedFive(){
+  var item2 = document.querySelector('h2.item5')
+  var date2 = document.querySelector('p.date5')
+
+  item2.setAttribute('class', 'itemDone')
+  date2.setAttribute('class', 'dateDone')
+}
+async function setAsPassedSix(){
+  var item2 = document.querySelector('h2.item6')
+  var date2 = document.querySelector('p.date6')
+
+  item2.setAttribute('class', 'itemDone')
+  date2.setAttribute('class', 'dateDone')
+}
+async function setAsPassedSeven(){
+  var item2 = document.querySelector('h2.item7')
+  var date2 = document.querySelector('p.date7')
+
+  item2.setAttribute('class', 'itemDone')
+  date2.setAttribute('class', 'dateDone')
+}
+async function setAsPassedHeight(){
+  var item2 = document.querySelector('h2.item8')
+  var date2 = document.querySelector('p.date8')
+
+  item2.setAttribute('class', 'itemDone')
+  date2.setAttribute('class', 'dateDone')
+}
+async function setAsPassedNine(){
+  var item2 = document.querySelector('h2.item9')
+  var date2 = document.querySelector('p.date9')
+
+  item2.setAttribute('class', 'itemDone')
+  date2.setAttribute('class', 'dateDone')
+}
+async function setAsPassedTen(){
+  var item2 = document.querySelector('h2.item10')
+  var date2 = document.querySelector('p.date10')
+
+  item2.setAttribute('class', 'itemDone')
+  date2.setAttribute('class', 'dateDone')
+}
+async function setAsPassedEleven(){
+  var item2 = document.querySelector('h2.item11')
+  var date2 = document.querySelector('p.date11')
+
+  item2.setAttribute('class', 'itemDone')
+  date2.setAttribute('class', 'dateDone')
+}
+
+
+
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
@@ -37,6 +103,15 @@ function oneMonth(){
   const today = new Date().getTime();
   const dateOne = new Date(`April 06 2021 00:00:00 `).getTime();
   const dateTwo = new Date(`May 06 2021 00:00:00 `).getTime();
+  const dateTree = new Date(`June 06 2021 00:00:00 `).getTime();
+  const dateFour= new Date(`July 06 2021 00:00:00 `).getTime();
+  const dateFive = new Date(`August 06 2021 00:00:00 `).getTime();
+  const dateSix = new Date(`September 06 2021 00:00:00 `).getTime();
+  const dateSeven = new Date(`October 06 2021 00:00:00 `).getTime();
+  const dateHeight = new Date(`November 06 2021 00:00:00 `).getTime();
+  const dateNine = new Date(`December 06 2021 00:00:00 `).getTime();
+  const dateTen = new Date(`January 06 2022 00:00:00 `).getTime();
+  const dateEleven = new Date(`February 06 2022 00:00:00 `).getTime();
 
   if(today >= dateOne){
     setAsPassedOne()
@@ -44,8 +119,36 @@ function oneMonth(){
   if(today >= dateTwo){
     setAsPassedTwo()
   }
+  if(today >= dateTree){
+    setAsPassedTree()
+  }
+  if(today >= dateFour){
+    setAsPassedFour()
+  }
+  if(today >= dateFive){
+    setAsPassedFive()
+  }
+  if(today >= dateSix){
+    setAsPassedSix()
+  }
+  if(today >= dateSeven){
+    setAsPassedSeven()
+  }
+  if(today >= dateHeight){
+    setAsPassedHeight()
+  }
+  if(today >= dateNine){
+    setAsPassedNine()
+  }
+  if(today >= dateTen){
+    setAsPassedTen()
+  }
+  if(today >= dateEleven){
+    setAsPassedEleven()
+  }
   return('06/04/2021')
-}oneMonth()
+}
+oneMonth()
 
 function daysTo(){
   const date_oneY = new Date(`March 06 2022 00:00:00`).getTime();
@@ -146,56 +249,56 @@ export default function Home() {
               </li>
               <li class='listItem'>
                 <div class='itemBox' >
-                  <h2 class='item2'>3 meses de namoro </h2>
-                  <p class='date2'>06/06/2021</p>
+                  <h2 class='item3'>3 meses de namoro </h2>
+                  <p class='date3'>06/06/2021</p>
                 </div>
               </li>
               <li class='listItem2'>
                 <div class='itemBox2' >
-                  <h2 class='item3'>4 meses de namoro </h2>
-                  <p class='date3'>06/07/2021</p>
+                  <h2 class='item4'>4 meses de namoro </h2>
+                  <p class='date4'>06/07/2021</p>
                 </div>
               </li>
               <li class='listItem2'>
                 <div class='itemBox2' >
-                  <h2 class='item3'>5 meses de namoro </h2>
-                  <p class='date3'>06/08/2021</p>
+                  <h2 class='item5'>5 meses de namoro </h2>
+                  <p class='date5'>06/08/2021</p>
                 </div>
               </li>
               <li class='listItem2'>
                 <div class='itemBox2' >
-                  <h2 class='item3'>6 meses de namoro </h2>
-                  <p class='date3'>06/09/2021</p>
+                  <h2 class='item6'>6 meses de namoro </h2>
+                  <p class='date6'>06/09/2021</p>
                 </div>
               </li>
               <li class='listItem2'>
                 <div class='itemBox2' >
-                  <h2 class='item3'>7 meses de namoro </h2>
-                  <p class='date3'>06/10/2021</p>
+                  <h2 class='item7'>7 meses de namoro </h2>
+                  <p class='date7'>06/10/2021</p>
                 </div>
               </li>
               <li class='listItem2'>
                 <div class='itemBox2' >
-                  <h2 class='item3'>8 meses de namoro </h2>
-                  <p class='date3'>06/11/2021</p>
+                  <h2 class='item8'>8 meses de namoro </h2>
+                  <p class='date8'>06/11/2021</p>
                 </div>
               </li>
               <li class='listItem2'>
                 <div class='itemBox2' >
-                  <h2 class='item3'>9 meses de namoro </h2>
-                  <p class='date3'>06/12/2021</p>
+                  <h2 class='item9'>9 meses de namoro </h2>
+                  <p class='date9'>06/12/2021</p>
                 </div>
               </li>
               <li class='listItem2'>
                 <div class='itemBox2' >
-                  <h2 class='item3'>10 meses de namoro </h2>
-                  <p class='date3'>06/01/2022</p>
+                  <h2 class='item10'>10 meses de namoro </h2>
+                  <p class='date10'>06/01/2022</p>
                 </div>
               </li>
               <li class='listItem2'>
                 <div class='itemBox2' >
-                  <h2 class='item3'>11 meses de namoro </h2>
-                  <p class='date3'>06/02/2022</p>
+                  <h2 class='item11'>11 meses de namoro </h2>
+                  <p class='date11'>06/02/2022</p>
                 </div>
               </li>
               <li class='listItemYear'>
