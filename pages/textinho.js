baf3b5ed40e33e7246a2b62aec2aeacaeb824f5c
FranKeN Pereira <frankenpereira@gmail.com>
2021-07-28 23:33:07 +0100

    mozao

--- pages/textinho.js
+++ pages/textinho.js
@@ -11,7 +11,7 @@ export default function Birthday(){
                         <h1 class='bdTitle'>Parabéns Mozão ❤</h1>
                         <h2></h2>
                     </div>
-                    <div class='levels'>
+                    <div class='textinhoMain'>
                         <p class='textinho'>Oii amor, vida, bebé, princesa, QUEEN, meu TUDO e PARABÉNSSSS já são 17 aninhos e como tu me disseste espero que os próximos sejam comigo hihihi.</p>
                         <p class='textinho'>Quero que saibas que eu te amo MUITO mas mesmo MUITOOOO, nunca amei ninguém como te amo a ti, tu és o meu mundo, o meu universo, sei que não sou facil de aturar porque sou muito ciumento mas eu tenho tentado melhorar isso e não só isso tenho tentado melhorar tudo o que eu posso pela nossa relação porque eu amo-te mesmo muito e nossa relação é mais importante para mim do qualquer mania ou feitio que eu tenha!</p>
                         <p class='textinho'> Quero que sabias que me fazes o rapaz mais feliz do UNIVERSO, só o simples facto de estares comigo já me faz muito feliz e melhora o meu dia a INFINITOS porcento, tu és incrível, maravilhosa, perfeita, espantosa e tudo de bom. És mesmo o amor da minha VIDAAA. Também quero que saibas que estou sempre aqui para ti e sempre estarei aqui para ti, tu és a minha esposa hihihi, estou aqui para ti na saúde e na doença. </p>
