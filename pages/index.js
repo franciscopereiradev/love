a62e0952eb8a60d91d16ab2eeadcfe7a783a68f9
Francisco Pereira <frankenpereira@gmail.com>
2021-10-03 20:14:13 +0100

    dark theme and some new features

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ import EventsPage from '../source/components/EventsPage'
 import { FaHome, FaHeart, FaGift } from "react-icons/fa";
 import { HiMenu } from 'react-icons/hi'
 import { BiExit, BiMenu } from 'react-icons/bi'
+import { IoInvertModeOutline, IoInvertMode } from 'react-icons/io5'
+import { Children } from 'react'
 
 async function changeToMainBD(){
   var mainBD = document.querySelector('div#mainBirthday')
@@ -168,6 +170,49 @@ function showMenu(){
 
 }
 
+async function changeTheme(){
+  var mainLogin = document.querySelector('div#mainLogin')
+  var mainBD = document.querySelector('div#mainBirthday')
+  var mainHome = document.querySelector('div#mainHome')
+  var mainReward = document.querySelector('div#mainReward')
+
+  if(localStorage.theme == 'light'){
+    mainLogin.style.background = 'linear-gradient(-45deg, rgb(153, 131, 158), rgb(88, 115, 139))'
+    mainBD.style.background = 'linear-gradient(-45deg, rgb(153, 131, 158), rgb(88, 115, 139))'
+    mainHome.style.background = 'linear-gradient(-45deg, rgb(153, 131, 158), rgb(88, 115, 139))'
+    mainReward.style.background = 'linear-gradient(-45deg, rgb(153, 131, 158), rgb(88, 115, 139))'
+    
+    localStorage.setItem('theme', 'dark')
+  } else if(localStorage.theme == 'dark'){
+    mainLogin.style.background = 'linear-gradient(-45deg, rgb(134, 194, 218), rgb(211, 156, 177))'
+    mainBD.style.background = 'linear-gradient(-45deg, rgb(134, 194, 218), rgb(211, 156, 177))'
+    mainHome.style.background = 'linear-gradient(-45deg, rgb(134, 194, 218), rgb(211, 156, 177))'
+    mainReward.style.background = 'linear-gradient(-45deg, rgb(134, 194, 218), rgb(211, 156, 177))'
+    
+    localStorage.theme = 'light'
+  }
+
+}
+
+function verifyTheme(){
+  var mainLogin = document.querySelector('div#mainLogin')
+  var mainBD = document.querySelector('div#mainBirthday')
+  var mainHome = document.querySelector('div#mainHome')
+  var mainReward = document.querySelector('div#mainReward')
+
+  if(localStorage.theme == 'light'){
+    mainLogin.style.background = 'linear-gradient(-45deg, rgb(153, 131, 158), rgb(88, 115, 139))'
+    mainBD.style.background = 'linear-gradient(-45deg, rgb(153, 131, 158), rgb(88, 115, 139))'
+    mainHome.style.background = 'linear-gradient(-45deg, rgb(153, 131, 158), rgb(88, 115, 139))'
+    mainReward.style.background = 'linear-gradient(-45deg, rgb(153, 131, 158), rgb(88, 115, 139))'
+    
+  } else if(localStorage.theme == 'dark'){
+    mainLogin.style.background = 'linear-gradient(-45deg, rgb(134, 194, 218), rgb(211, 156, 177))'
+    mainBD.style.background = 'linear-gradient(-45deg, rgb(134, 194, 218), rgb(211, 156, 177))'
+    mainHome.style.background = 'linear-gradient(-45deg, rgb(134, 194, 218), rgb(211, 156, 177))'
+    mainReward.style.background = 'linear-gradient(-45deg, rgb(134, 194, 218), rgb(211, 156, 177))'
+  }
+}verifyTheme()
 
 export default function Home() {
   
@@ -213,6 +258,9 @@ export default function Home() {
 
       <footer id='footer'>
         <div class='footerMain'>
+          <button id='modeBt' class='pageNotCorrent' onClick={changeTheme}>
+            <p><IoInvertModeOutline/></p>
+          </button>
           <button id='rewardBt' class='pageNotCorrent' onClick={changeToMainReward}>
             <p><FaGift/></p>
           </button>
