1c62ccc84f12f5c6c42aab5109602c0c2127c11d
Francisco Pereira <frankenpereira@gmail.com>
2021-10-12 19:17:07 +0100

    movies page preview

--- source/components/MoviePage/index.js
+++ source/components/MoviePage/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { FaPlus } from 'react-icons/fa'
+import { MdDone, MdDoneAll } from 'react-icons/md'
 
 function indisponivel(){
-    alert('Temporariamente indisponivel!')
+    alert('Temporariamente indisponível!')
 }
 
 function MoviePage(){
@@ -13,7 +14,7 @@ function MoviePage(){
 
     if(loading){
         return(  
-            <div class='container'>
+            <div class='containerMovies'>
                 <div class='circle1'></div>
                 <div class='circle2'></div>
                 <div class='glassBD'>
@@ -27,16 +28,135 @@ function MoviePage(){
     }
 
     return(  
-        <div class='container'>
+        <div class='container2'>
             <div class='circle1'></div>
             <div class='circle2'></div>
             <div class='glassBD'>
-                <h2>Filmes e series</h2>
-                <main style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <p>Ainda não tens nenhum filme ou serie!</p>
-                    <button id='addMore' class='pageNotCorrent' onClick={indisponivel}>
+                {/* <h2>Filmes e series</h2> */}
+                    {/* <button id='addMore' class='pageNotCorrent' onClick={indisponivel}>
                         <p><FaPlus/></p>
-                    </button>
+                    </button> */}
+                <main style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <p>Filmes</p>
+                    <section class='movies'>
+                        <div class='cardRow'>
+                            <div class='movieCard'></div>
+                            <div class='movieCard'></div>
+                            <div class='movieCard'></div>
+                            <div class='movieCard'></div>
+                        </div>
+                    </section>
+                        <p>Series</p>
+                    <section class='series'>
+                        <div class='cardRow'>
+                            <div class='serieCard'>
+                                <h2 class='cardTitle'>How I met your mother</h2>
+                                <p class='cardNext'>Proximo Episodio:</p>
+                                <p class='cardEp'>T: x Ep: y</p>
+                                <div class='cardFooter'>
+                                    <button class='cardBtn'>
+                                        <MdDone/> 
+                                    </button>
+                                    <button class='cardBtn'>
+                                        <MdDoneAll/> 
+                                    </button>
+                                </div>
+                            </div>
+                            <div class='serieCard'>
+                                <h2 class='cardTitle'>You</h2>
+                                <p class='cardNext'>Proximo Episodio:</p>
+                                <p class='cardEp'>T: x Ep: y</p>
+                                <div class='cardFooter'>
+                                    <button class='cardBtn'>
+                                        <MdDone/> 
+                                    </button>
+                                    <button class='cardBtn'>
+                                        <MdDoneAll/> 
+                                    </button>
+                                </div>
+                            </div>
+                            <div class='serieCard'>
+                                <h2 class='cardTitle'>Elite</h2>
+                                <p class='cardNext'>Proximo Episodio:</p>
+                                <p class='cardEp'>T: x Ep: y</p>
+                                <div class='cardFooter'>
+                                    <button class='cardBtn'>
+                                        <MdDone/> 
+                                    </button>
+                                    <button class='cardBtn'>
+                                        <MdDoneAll/> 
+                                    </button>
+                                </div>
+                            </div>
+                            <div class='serieCard'>
+                                <h2 class='cardTitle'>La casa de papel</h2>
+                                <p class='cardNext'>Proximo Episodio:</p>
+                                <p class='cardEp'>T: x Ep: y</p>
+                                <div class='cardFooter'>
+                                    <button class='cardBtn'>
+                                        <MdDone/> 
+                                    </button>
+                                    <button class='cardBtn'>
+                                        <MdDoneAll/> 
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='cardRow'>
+                            <div class='serieCard'>
+                                <h2 class='cardTitle'>Stranger things</h2>
+                                <p class='cardNext'>Proximo Episodio:</p>
+                                <p class='cardEp'>T: x Ep: y</p>
+                                <div class='cardFooter'>
+                                    <button class='cardBtn'>
+                                        <MdDone/> 
+                                    </button>
+                                    <button class='cardBtn'>
+                                        <MdDoneAll/> 
+                                    </button>
+                                </div>
+                            </div>
+                            <div class='serieCard'>
+                                <h2 class='cardTitle'>Outer banks</h2>
+                                <p class='cardNext'>Proximo Episodio:</p>
+                                <p class='cardEp'>T: x Ep: y</p>
+                                <div class='cardFooter'>
+                                    <button class='cardBtn'>
+                                        <MdDone/> 
+                                    </button>
+                                    <button class='cardBtn'>
+                                        <MdDoneAll/> 
+                                    </button>
+                                </div>
+                            </div>
+                            <div class='serieCard'>
+                                <h2 class='cardTitle'>9-1-1</h2>
+                                <p class='cardNext'>Proximo Episodio:</p>
+                                <p class='cardEp'>T: 5 Ep: 4</p>
+                                <div class='cardFooter'>
+                                    <button class='cardBtn'>
+                                        <MdDone/> 
+                                    </button>
+                                    <button class='cardBtn'>
+                                        <MdDoneAll/> 
+                                    </button>
+                                </div>
+                            </div>
+                            <div class='serieCard'>
+                                <h2 class='cardTitle'>Modern Family</h2>
+                                <p class='cardNext'>Proximo Episodio:</p>
+                                <p class='cardEp'>T: x Ep: y</p>
+                                <div class='cardFooter'>
+                                    <button class='cardBtn'>
+                                        <MdDone/> 
+                                    </button>
+                                    <button class='cardBtn'>
+                                        <MdDoneAll/> 
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </main>
             </div>
         </div>
