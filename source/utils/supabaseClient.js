7a9baeee77d835073efd60e05c2d7526a7c1a15b
Francisco Pereira <frankenpereira@gmail.com>
2021-10-22 19:25:38 +0100

    events working

--- source/utils/supabaseClient.js
+++ source/utils/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient('https://dxqpklqekjbdfovaspej.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDkxOTUyNywiZXhwIjoxOTUwNDk1NTI3fQ.8nXAKrvcNuvnLVbiNov3gSqbVsu2LIzz73plgmFNTkQ')
